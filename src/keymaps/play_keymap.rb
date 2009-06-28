89ab3ee2b6db7e8ecf81e71847a925232380067c
Jay McGavren <jay@mcgavren.com>
2009-06-28 00:15:33 -0700

    Added README.textile

--- src/keymaps/play_keymap.rb
+++ src/keymaps/play_keymap.rb
@@ -4,6 +4,9 @@ Gemini::InputManager.define_keymap do |i|
 
   0.upto i.connected_joystick_size - 1 do |j|
     i.map_joystick :joystick_id => j, :released => XBOX_360_A, :draw_mark => nil, :player => j
+    i.map_joystick :joystick_id => j, :released => XBOX_360_B, :draw_mark => nil, :player => j
+    i.map_joystick :joystick_id => j, :released => XBOX_360_X, :draw_mark => nil, :player => j
+    i.map_joystick :joystick_id => j, :released => XBOX_360_Y, :draw_mark => nil, :player => j
     i.map_joystick :joystick_id => j, :held => XBOX_360_DPAD_UP, :change_y => nil, :player => j do |message, raw_input|
       message.value = -1
     end
